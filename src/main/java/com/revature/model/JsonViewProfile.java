1d37ae5c69942577c3bed6c5a2f2f47b7ca85171
ZGApps <appletonzak@gmail.com>
2022-01-13 22:56:34 -0600

    Book model and prospective JsonViewProfile

---
+++ src/main/java/com/revature/model/JsonViewProfile.java
@@ -0,0 +1,18 @@
+package com.revature.model;
+
+public class JsonViewProfile {
+
+	
+	
+	public static class User{
+		
+	}
+	
+	public static class Book {
+		
+	}
+	
+	public static class Catagories{
+		
+	}
+}
