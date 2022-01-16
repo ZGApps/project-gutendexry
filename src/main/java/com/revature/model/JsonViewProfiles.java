64feaebb997666a9bd75b0ae85ffd73661a877ea
ZGApps <appletonzak@gmail.com>
2022-01-16 12:59:05 -0600

    Update JsonViewProfile to match JsonViewProfiles in main

---
+++ src/main/java/com/revature/model/JsonViewProfiles.java
@@ -0,0 +1,22 @@
+package com.revature.model;
+
+
+	public class JsonViewProfiles {
+		
+		
+		public static class User {
+			
+		}
+		
+		public static class Book {
+			
+			
+		}
+		
+		public static class Category {
+			
+			
+		}
+		
+
+	}
