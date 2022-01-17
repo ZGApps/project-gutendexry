5acbc2e05e60ed29eda0d1ce2f3d1222cb5a79bd
jahred <jboyd4314@gmail.com>
2022-01-17 10:26:44 -0500

    update findByUsername and existsByUsername

--- src/main/java/com/revature/data/UserRepository.java
+++ src/main/java/com/revature/data/UserRepository.java
@@ -16,21 +16,6 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	
 	Optional<User> findByUsername(String username); 
 	
-	public List<User> findByOrderByLastName(); 
-	
-	public List<User> findByOrderByBook();
-	
-	public List<User> findByOrderByCategory();
-	
-	// TODO: modify this so that it returns either boolean or PK of the user deleted. 
-	public void deleteById(int id);
-	
-	
-	@Query("FROM User WHERE book LIKE %:pattern")
-	public List<User> findByBookContains(String pattern);
-	
-	@Query("FROM User WHERE category LIKE %:pattern")
-	public List<User> findByCategoryContains(String pattern); 
-	
-	// TODO add customer queries findByBookOrderByLastName
+	public boolean existsByUsername(String username);
+
 }
