b4592d56efac9cabe055bcd52b94a1bb1437c6e2
vishalathar <atharvishal@gmail.com>
2022-01-23 14:17:08 -0500

    No Changes on my end

--- src/main/java/com/revature/data/BookRepository.java
+++ src/main/java/com/revature/data/BookRepository.java
@@ -1,16 +1,18 @@
 package com.revature.data;
 
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.model.Book;
+import com.revature.model.User;
 
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer>{
 	
 	public Set<Book> findByOwnersUsername(String username);
-
 }
